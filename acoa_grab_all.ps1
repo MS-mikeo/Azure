235d48efe7959f4764d9c6bf098eb7c2b78a3d37
Mike O'Shaughnessy <130005316+MS-mikeo@users.noreply.github.com>
2024-08-07 12:03:17 -0400

    Update acoa_grab_all.ps1

--- acoa_grab_all.ps1
+++ acoa_grab_all.ps1
@@ -1,3 +1,19 @@
+# ===========================================================================================
+#
+# This sample script is not supported under any Microsoft standard support program or service. 
+# The sample script is provided AS IS without warranty of any kind. Microsoft further disclaims 
+# all implied warranties including, without limitation, any implied warranties of merchantability 
+# or of fitness for a particular purpose. The entire risk arising out of the use or performance of 
+# the sample scripts and documentation remains with you. In no event shall Microsoft, its authors, 
+# or anyone else involved in the creation, production, or delivery of the scripts be liable for any 
+# damages whatsoever (including, without limitation, damages for loss of business profits, business 
+# interruption, loss of business information, or other pecuniary loss) arising out of the use of or 
+# inability to use the sample scripts or documentation, even if Microsoft has been advised of the 
+# possibility of such damages 
+#
+# ===========================================================================================
+
+
 # Changes needed before finished:
 # 1.) Update all Subscription IDs to have Subscription name in any GENERAL and COMPUTE exports
 # 2.) Add Advisor and Orphan resources export (and any other beneficial ones - empty app service plans that are not serverless)
